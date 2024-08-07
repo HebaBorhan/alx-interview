a8d733954a80e09730b76e6765f7e0afbc9b700c
HebaZaki <h_borhan@hotmail.com>
2024-08-07 04:23:36 +0200

    min operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,17 +3,18 @@
 
 
 def minOperations(n):
-    """Return the minimum number of operations to result n H characters in text file"""
+    """Return minimum num of operations
+    to result n H characters in text file"""
     if n <= 1:
         return 0
-    
+
     numberOperations = 0
     primeFactor = 2
-    
+
     while n > 1:
         while n % primeFactor == 0:
             numberOperations += primeFactor
             n = n // primeFactor
         primeFactor += 1
-    
+
     return numberOperations
