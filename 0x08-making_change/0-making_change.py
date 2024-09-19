dd31039b5573135ae73158408cf1ecf112f176b3
HebaZaki <h_borhan@hotmail.com>
2024-09-19 13:10:31 +0300

    fewer num coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,15 +7,15 @@ def makeChange(coins, total):
     needed to meet a given amount total"""
     if total <= 0:
         return 0
-    
+
     # Initialize dp array with infinity for all totals except 0
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     # Fill dp array
     for coin in coins:
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
-    
+
     # If dp[total] infinity, impossible to form total
     return dp[total] if dp[total] != float('inf') else -1
