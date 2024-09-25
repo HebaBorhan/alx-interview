0f14150bfe6aedfdce265cba00065ea53483a186
HebaZaki <h_borhan@hotmail.com>
2024-09-25 09:57:36 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""island perimeter"""
+
+def island_perimeter(grid):
+    """eturns the perimeter of the island described in grid"""
+    # Grid dimensions
+    rows = len(grid)
+    cols = len(grid[0])
+
+    perimeter = 0
+
+    # Traverse each cell in grid
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == 1:
+                # Start with 4 for each land cell
+                perimeter += 4
+
+                # Check if there is land above
+                if r > 0 and grid[r-1][c] == 1:
+                    perimeter -= 1
+
+                # Check if there is land below
+                if r < rows - 1 and grid[r+1][c] == 1:
+                    perimeter -= 1
+
+                # Check if there is land to the left
+                if c > 0 and grid[r][c-1] == 1:
+                    perimeter -= 1
+
+                # Check if there is land to the right
+                if c < cols - 1 and grid[r][c+1] == 1:
+                    perimeter -= 1
+
+    return perimeter
