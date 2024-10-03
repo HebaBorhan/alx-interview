634ea63766dc98c92d569a62dcf628557d0eb677
HebaZaki <h_borhan@hotmail.com>
2024-10-03 13:40:03 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,23 +7,23 @@ def isWinner(x, nums):
     if not nums or x < 1:
         return None
 
-    # Determine the maximum value of n we will deal with
+    # Maximum value of n
     max_n = max(nums)
 
-    # Step 1: Sieve of Eratosthenes to determine prime numbers up to max_n
+    # Sieve of Eratosthenes
     sieve = [True] * (max_n + 1)
-    sieve[0] = sieve[1] = False  # 0 and 1 are not primes
+    sieve[0] = sieve[1] = False
     for i in range(2, int(max_n ** 0.5) + 1):
         if sieve[i]:
             for j in range(i * i, max_n + 1, i):
                 sieve[j] = False
 
-    # Step 2: Create a list to keep track of the number of primes <= n
+    # Creating list to keep track of number of primes <= n
     prime_count = [0] * (max_n + 1)
     for i in range(1, max_n + 1):
         prime_count[i] = prime_count[i - 1] + (1 if sieve[i] else 0)
 
-    # Step 3: Simulate the rounds
+    # Simulating rounds
     maria_wins = 0
     ben_wins = 0
 
@@ -33,7 +33,7 @@ def isWinner(x, nums):
         else:
             maria_wins += 1
 
-    # Step 4: Determine the winner
+    # Determinening the winner
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
